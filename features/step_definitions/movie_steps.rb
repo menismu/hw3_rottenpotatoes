9bf540482f47bdd37c8fe1c1d9cd799053315a5d
menismu <menismu@gmail.com>
2013-02-11 14:31:06 -0800

    Homework 3 part c

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -25,4 +25,15 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(', ').each do |rating|
+    When %{I #{uncheck}check "ratings_#{rating}"} 
+  end
+end
+
+Then /I should see all of the movies/ do
+  assert page.all("table tbody tr").count == 10
+end
+
+Then /I should not see movies/ do
+  assert page.all("table tbody tr").count == 0
 end
